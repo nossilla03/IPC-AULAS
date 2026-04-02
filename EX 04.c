c9d4d06249c0e267b17db4e1bc3af6011fb2ea2a
nossilla03 <allissonkauasantosrocha@gmail.com>
2026-04-01 21:02:07 -0300

    Create EX 04.c

---
+++ EX 04.c
@@ -0,0 +1,22 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+int valor;
+printf ("digite o valor:\n");
+scanf ("%d", &valor);
+
+if (valor <= 10) {
+    printf ("categoria A");
+ } 
+ else if (valor <= 20) {
+     printf ("categoria B");
+ } 
+ else { 
+     printf ("categoria c");
+ } 
+ 
+ return 0;
+     
+    
+}
